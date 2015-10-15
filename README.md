f4eca2d9338dd93ac203f609138fb14635d2b820
Stefano D'Angelo <zanga.mail@gmail.com>
2015-10-15 22:23:13 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# virtualPianoKeyboard.js
+Minimal HTML5 Canvas/JavaScript piano keyboard.
+
+I am too lazy to explain hows and whys, just take a look at the example files:
+* virtualPianoKeyboard - 25 keys piano keyboard;
+* trivialSynth - trivial monophonic synth (Web Audio API based).
+
+## TODO:
+
+* add touch events;
+* add Web MIDI API events;
+* use polyfills where needed?
+* review API;
+* better graphics;
+* documentation.
---
+++ mouse.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 Stefano D'Angelo <zanga.mail@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+virtualPianoKeyboard.keyboard.addMouseIn = function (element, canvas) {
+	var k = this;
+	var keyCur = null;
+	var keyCurHover = false;
+	var mouseDown = false;
+
+	canvas.addEventListener("mousemove",
+		function (event) {
+			var rect = canvas.getBoundingClientRect();
+			var x = event.clientX - rect.left; 
+			var y = event.clientY - rect.top;
+			var key = k.getKeyByCoordinates(x, y);
+
+			if (key != keyCur) {
+				// this first for legato
+				if (key) {
+					k.keyHoverIn(key, true);
+					if (mouseDown)
+						k.keyPressIn(key, true);
+				}
+
+				if (keyCur) {
+					if (keyCurHover)
+						k.keyHoverIn(keyCur, false);
+					if (mouseDown)
+						k.keyPressIn(keyCur, false);
+				}
+
+				keyCur = key;
+			} else if (!keyCurHover)
+				k.keyHoverIn(keyCur, true);
+
+			keyCurHover = true;
+		});
+
+	canvas.addEventListener("mouseout",
+		function (event) {
+			if (keyCur)
+				k.keyHoverIn(keyCur, false);
+			keyCurHover = false;
+		});
+
+	canvas.addEventListener("mousedown",
+		function (event) {
+			if (event.button == 0) {
+				if (keyCur)
+					k.keyPressIn(keyCur, true);
+				mouseDown = true;
+			}
+		});
+
+	element.addEventListener("mouseup",
+		function (event) {
+			if (event.button == 0) {
+				if (keyCur && mouseDown)
+					k.keyPressIn(keyCur, false);
+				mouseDown = false;
+			}
+		});
+};
